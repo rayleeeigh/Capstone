2357283cf07fd510af32c95fb9ed7cfbd8fb7119
arnan123 <arnanplanco@gmail.com>
2022-04-12 18:55:29 +0800

    CRUD

--- client/src/components/announcement/AnnouncementCard.js
+++ client/src/components/announcement/AnnouncementCard.js
@@ -1,20 +1,30 @@
-import * as React from "react";
+import  React,{useState} from "react";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import { Avatar, Badge, CardHeader } from "@mui/material";
-import {deleteDoc, doc} from 'firebase/firestore';
+import { Avatar, Badge, CardHeader, TextField } from "@mui/material";
+import {deleteDoc, doc,updateDoc} from 'firebase/firestore';
 import { db, auth, storage } from '../../firebase';
 
 export default function AnnouncementCard({content,getAll}) {
+  const [updated,setUpdated]=useState('');
 
   const deleteData = async (id) =>{
-    console.log(id);
     const annDelete = doc(db, "announcements", id);
     await deleteDoc(annDelete);
+    getAll();
+  }
+
+  const updateData = async (id) =>{
+    const annUpdate = doc(db, "announcements", id);
+    content.content=updated;
+    console.log(content.content);
+    await updateDoc(annUpdate,content);
+    
+    getAll();
   }
 
   return (
@@ -40,10 +50,12 @@ export default function AnnouncementCard({content,getAll}) {
             <br />
             {'"a benevolent smile"'}
           </Typography>
+          <TextField value={updated} onChange={(e)=>{setUpdated(e.target.value)}}>Update Content</TextField>
         </CardContent>
         <CardActions>
           <Button size='small'>See Full Announcement</Button>
-          <Button size='small' onClick={(e)=>{deleteData(content.id).then((res)=>{console.log('haha')})}}>Delete Announcement </Button>
+          <Button size='small' onClick={(e)=>{deleteData(content.id)}}>Delete Announcement </Button>
+          <Button size='small' onClick={(e)=>{updateData(content.id)}}>Update Announcement </Button>
         </CardActions>
       </Card>
     </Box>
