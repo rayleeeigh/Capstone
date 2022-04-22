2a297abaeb1e0a681fee9be6461af2fc5b290579
arnan123 <arnanplanco@gmail.com>
2022-04-22 17:18:23 +0800

    Updated CODEOWNERS and Modal

--- client/src/components/styled/Navbar.js
+++ client/src/components/styled/Navbar.js
@@ -1,209 +1,234 @@
-import Avatar from "@mui/material/Avatar";
-import React, { useState } from "react";
-import styled from "styled-components";
-import SchoolLogo from "../../assets/buyongLogo.png";
-import NotificationsIcon from "@mui/icons-material/Notifications";
+import Avatar from '@mui/material/Avatar'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import SchoolLogo from '../../assets/buyongLogo.png'
+import NotificationsIcon from '@mui/icons-material/Notifications'
 import {
-  Badge,
-  Box,
-  FormControl,
-  IconButton,
-  InputLabel,
-  MenuItem,
-  Select,
-  Stack,Menu
-} from "@mui/material";
-import { auth } from "../../firebase";
-import { signOut } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
+    Badge,
+    Box,
+    FormControl,
+    IconButton,
+    InputLabel,
+    MenuItem,
+    Select,
+    Stack,
+    Menu,
+    Typography,
+} from '@mui/material'
+import { auth } from '../../firebase'
+import { signOut } from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
+import ModalContent from './ModalProfile'
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-  const history = useNavigate();
+    const [isOpen, setIsOpen] = useState(false)
+    const [anchorEl, setAnchorEl] = React.useState(null)
+    const [opens, setOpens] = useState(false)
+    const open = Boolean(anchorEl)
+    const history = useNavigate()
 
-  const handleClick = (e) => {
-    setAnchorEl(e.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-  const handleSignout = async () => {
-    handleClose();
-    await signOut(auth);
-    history("/");
-  };
-  return (
-    <Nav>
-      <Logo className='Logo' href='/home'>
-        <div>
-          <img className='img' src={SchoolLogo} alt='School Logo' />
-        </div>
-        <div className='text'>BUYONG NATIONAL HIGHSCHOOL</div>
-      </Logo>
-      <Hamburger onClick={() => setIsOpen(!isOpen)}>
-        <span />
-        <span />
-        <span />
-      </Hamburger>
-      <Menus isOpen={isOpen}>
-        <MenuLink href='/home'>Announcements</MenuLink>
-        <MenuLink href='/about'>About</MenuLink>
-        <MenuLink href=''>Faculty</MenuLink>
-        <FormControl  sx={{ m: 1, minWidth: "9rem" }}>
-          <InputLabel>Student Task</InputLabel>
-          <Select>
-            <a href='/grades'>
-              <MenuItem>Grades</MenuItem>
-            </a>
-            <a href='/schedule'>
-              <MenuItem>Schedule</MenuItem>
-            </a>
-            <a href='/admin'>
-              <MenuItem>Admin Dashboard</MenuItem>
-            </a>
-            <a href='/admin/accountverification'>
-              <MenuItem>Account Verifications</MenuItem>
-            </a>
-            <a href='/admin/logintrail'>
-              <MenuItem>Login Trail</MenuItem>
-            </a>
-          </Select>
-        </FormControl>
-        <Stack direction={"row"} spacing={3}>
-          <IconButton>
-            <Box>
-              <Badge color='primary' badgeContent={0} showZero>
-                <NotificationsIcon sx={{ width: "30px", height: "30px" }} />
-              </Badge>
-            </Box>
-          </IconButton>
-          <IconButton  id="basic-button"
-            aria-controls={open ? 'basic-menu' : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? 'true' : undefined}
-            onClick={handleClick}>
-            <Avatar />
-          </IconButton>
-            <Menu
-              id="basic-menu"
-              anchorEl={anchorEl}
-              open={open}
-              onClose={handleClose}
-              MenuListProps={{
-                'aria-labelledby': 'basic-button',
-              }}
-            >
-              <MenuItem onClick={handleClose}>Profile</MenuItem>
-              <MenuItem onClick={handleClose}>My account</MenuItem>
-              <MenuItem onClick={handleSignout}>Logout</MenuItem>
-            </Menu>
-          
-        </Stack>
-      </Menus>
-    </Nav>
-  );
-};
+    const handleClick = (e) => {
+        setAnchorEl(e.currentTarget)
+    }
+    const handleClose = () => {
+        setAnchorEl(null)
+    }
+    const handleSignout = async () => {
+        handleClose()
+        await signOut(auth)
+        history('/')
+    }
+    return (
+        <Nav>
+            <Logo className="Logo" href="/home">
+                <div>
+                    <img className="img" src={SchoolLogo} alt="School Logo" />
+                </div>
+                <div className="text">BUYONG NATIONAL HIGHSCHOOL</div>
+            </Logo>
+            <Hamburger onClick={() => setIsOpen(!isOpen)}>
+                <span />
+                <span />
+                <span />
+            </Hamburger>
+            <Menus isOpen={isOpen}>
+                <MenuLink href="/home">Announcements</MenuLink>
+                <MenuLink href="/about">About</MenuLink>
+                <MenuLink href="">Faculty</MenuLink>
+                <FormControl sx={{ m: 1, minWidth: '9rem' }}>
+                    <InputLabel>Student Task</InputLabel>
+                    <Select>
+                        <a href="/grades">
+                            <MenuItem>Grades</MenuItem>
+                        </a>
+                        <a href="/schedule">
+                            <MenuItem>Schedule</MenuItem>
+                        </a>
+                        <a href="/admin">
+                            <MenuItem>Admin Dashboard</MenuItem>
+                        </a>
+                        <a href="/admin/accountverification">
+                            <MenuItem>Account Verifications</MenuItem>
+                        </a>
+                        <a href="/admin/logintrail">
+                            <MenuItem>Login Trail</MenuItem>
+                        </a>
+                    </Select>
+                </FormControl>
+                <Stack direction={'row'} spacing={3}>
+                    <IconButton>
+                        <Box>
+                            <Badge color="primary" badgeContent={0} showZero>
+                                <NotificationsIcon
+                                    sx={{ width: '30px', height: '30px' }}
+                                />
+                            </Badge>
+                        </Box>
+                    </IconButton>
+                    <IconButton
+                        id="basic-button"
+                        aria-controls={open ? 'basic-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}
+                        onClick={handleClick}
+                    >
+                        <Avatar />
+                    </IconButton>
 
-export default Navbar;
+                    <Menu
+                        id="basic-menu"
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+                        MenuListProps={{
+                            'aria-labelledby': 'basic-button',
+                        }}
+                    >
+                        <MenuItem
+                            onClick={handleClose}
+                            onClickCapture={() => setOpens(true)}
+                        >
+                            Profile
+                        </MenuItem>
+                        <MenuItem onClick={handleClose}>My account</MenuItem>
+                        <MenuItem onClick={handleSignout}>Logout</MenuItem>
+                    </Menu>
+                    <ModalContent
+                        open={opens}
+                        handleClose={() => {
+                            setOpens(false)
+                        }}
+                        content={
+                            <Box>
+                                <Typography>Hatdog</Typography>
+                            </Box>
+                        }
+                        title="Add a section"
+                    />
+                </Stack>
+            </Menus>
+        </Nav>
+    )
+}
+
+export default Navbar
 
 const MenuLink = styled.a`
-  padding: 1rem 2rem;
-  cursor: pointer;
-  text-align: center;
-  text-decoration: none;
-  color: black;
-  transition: all 0.3s ease-in;
-  font-size: 0.9rem;
-  &:hover {
-    color: #67bc98;
-  }
-`;
+    padding: 1rem 2rem;
+    cursor: pointer;
+    text-align: center;
+    text-decoration: none;
+    color: black;
+    transition: all 0.3s ease-in;
+    font-size: 0.9rem;
+    &:hover {
+        color: #67bc98;
+    }
+`
 
 const Nav = styled.div`
-  padding: 0 2rem;
-  width: 100vw;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  flex-wrap: wrap;
-  background: ghostwhite;
-  position: sticky;
-  top: 0;
-  left: 0;
-  right: 0;
-  box-shadow: 4px 2px 2px black;
-  z-index: 5;
-`;
+    padding: 0 2rem;
+    width: 100vw;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+    background: ghostwhite;
+    position: sticky;
+    top: 0;
+    left: 0;
+    right: 0;
+    box-shadow: 4px 2px 2px black;
+    z-index: 5;
+`
 
 const Logo = styled.a`
-  padding: 1rem 0;
-  color: black;
-  text-decoration: none;
-  font-weight: 900 !important;
-  font-size: 1rem;
-  font-family: "lato", sans-serif;
-  display: flex;
-  span {
-    font-weight: 300;
-    font-size: 1.3rem;
-  }
-  .img {
-    width: 70px;
-  }
-  .text {
-    width: 80px;
-  }
-
-  div {
-    margin: 5px;
-  }
-
-  @media (max-width: 820px) {
-    .text {
-      display: none;
-    }
-  }
-`;
-
-const Menus = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  position: relative;
-  @media (max-width: 850px) {
-    overflow: hidden;
-    flex-direction: column;
-    max-height: ${({ isOpen }) => (isOpen ? "300px" : "0")};
-    transition: max-height 0.3s ease-in;
-    width: 100%;
-
-    .user {
-      display: none;
-    }
-  }
-
-  .Row {
-    width: 10vw;
-  }
-`;
-
-const Hamburger = styled.div`
-  display: none;
-  flex-direction: column;
-  cursor: pointer;
-  span {
-    height: 2px;
-    width: 25px;
-    background: #7b7fda;
-    margin-bottom: 4px;
-    border-radius: 5px;
-  }
-  @media (max-width: 820px) {
+    padding: 1rem 0;
+    color: black;
+    text-decoration: none;
+    font-weight: 900 !important;
+    font-size: 1rem;
+    font-family: 'lato', sans-serif;
     display: flex;
     span {
-      background-color: black;
+        font-weight: 300;
+        font-size: 1.3rem;
     }
-  }
-`;
+    .img {
+        width: 70px;
+    }
+    .text {
+        width: 80px;
+    }
+
+    div {
+        margin: 5px;
+    }
+
+    @media (max-width: 820px) {
+        .text {
+            display: none;
+        }
+    }
+`
+
+const Menus = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: relative;
+    @media (max-width: 850px) {
+        overflow: hidden;
+        flex-direction: column;
+        max-height: ${({ isOpen }) => (isOpen ? '300px' : '0')};
+        transition: max-height 0.3s ease-in;
+        width: 100%;
+
+        .user {
+            display: none;
+        }
+    }
+
+    .Row {
+        width: 10vw;
+    }
+`
+
+const Hamburger = styled.div`
+    display: none;
+    flex-direction: column;
+    cursor: pointer;
+    span {
+        height: 2px;
+        width: 25px;
+        background: #7b7fda;
+        margin-bottom: 4px;
+        border-radius: 5px;
+    }
+    @media (max-width: 820px) {
+        display: flex;
+        span {
+            background-color: black;
+        }
+    }
+`
