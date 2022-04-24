d03565411833552745a4e7bf90d39cf39c300a9b
arnan123 <arnanplanco@gmail.com>
2022-04-24 13:54:25 +0800

    stepper

---
+++ client/src/components/Signup/Signup.style.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+import { TextField } from '@mui/material'
+import MuiPhoneNumber from 'material-ui-phone-number'
+
+export const ITextField = styled(TextField)`
+    width: 20vw;
+`
+
+export const PTextField = styled(TextField)`
+    width: 30vw;
+`
+
+export const PhoneNumber = styled(MuiPhoneNumber)`
+    border: solid 1px silver;
+    padding: 10px;
+    border-radius: 5px;
+    margin-top: 16px;
+`
---
+++ client/src/pages/Signup.js
@@ -0,0 +1,45 @@
+import * as React from 'react'
+import { Typography, Stack, Avatar } from '@mui/material'
+import StepperSignup from '../components/Signup/StepperSignup'
+
+export default function SignUp() {
+    const [value, setValue] = React.useState(null)
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        const data = new FormData(event.currentTarget)
+        console.log({
+            email: data.get('email'),
+            password: data.get('password'),
+        })
+    }
+
+    return (
+        <>
+            <Stack
+                sx={{
+                    my: 8,
+                    mx: 32,
+                    display: 'flex',
+                    justifyContent: 'center',
+                }}
+            >
+                <Stack
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        flexDirection: 'column',
+                    }}
+                >
+                    <Avatar
+                        src="https://i.ibb.co/xFWmPt9/logo.png"
+                        sx={{ width: 80, height: 80 }}
+                    />
+                    <Typography component="h1" variant="h5">
+                        Buyong High School Admission Page
+                    </Typography>
+                </Stack>
+                <StepperSignup />
+            </Stack>
+        </>
+    )
+}
