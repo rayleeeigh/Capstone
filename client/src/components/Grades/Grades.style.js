ff135370cff172e0f728324fb219bee1c9721392
Rayl Xylem Regañon <raylxylem@gmail.com>
2022-07-07 10:12:21 +0800

    reset

--- client/src/components/Grades/Grades.style.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-import { TableCell } from '@mui/material';
-
-export const HeaderCell = styled(TableCell)`
-  background-color: #6495ED;
-  color: white;
-`;
