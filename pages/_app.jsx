1f8e385b711684627b6f5836dc56ab2599099766
arnan123 <arnanplanco@gmail.com>
2022-10-09 20:18:39 +0800

    added login

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,13 @@
+import { UserProvider } from '@auth0/nextjs-auth0';
 import { ChakraProvider } from '@chakra-ui/react';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <UserProvider>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </UserProvider>
   );
 }
 
