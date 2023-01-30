0256a7a049c1f422111fff833a42a366776fba20
Rayl Xylem Regañon <raylxylem@gmail.com>
2023-01-31 00:10:41 +0800

    revert unnecessary change

--- pages/api/announcements/postAnnouncements.ts
+++ pages/api/announcements/postAnnouncements.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { postAnnouncement } from '../../../lib/announcements';
 
-export default async function handler(req: NextApiRequest, res:NextApiResponse) {+
+export default async function handler(req: NextApiRequest, res:NextApiResponse) {
   const announcements = await postAnnouncement();
   return res.status(200).end();
 }
