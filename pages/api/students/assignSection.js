a622c57f40afd969bbb6eeb613f0486c0a120d55
Rayl Xylem Regañon <raylxylem@gmail.com>
2023-04-11 21:19:22 +0800

    enroll v2

--- pages/api/students/assignSection.js
+++ pages/api/students/assignSection.js
@@ -2,8 +2,7 @@ import { assignSection } from '../../../lib/students';
 import { getSections } from '../../../lib/sections';
 
 export default async function handler(req, res) {
-  const { array, students }= req.body;
-  const sections = await getSections()
+  const { array, students, sections }= req.body;
   await assignSection(array, students, sections);
   return res.status(200).json();
 }
