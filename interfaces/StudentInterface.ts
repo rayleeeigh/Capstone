aef98321f79a379b0f82729bed67329edf0d1449
arnan123 <arnanplanco@gmail.com>
2023-05-22 19:12:14 +0800

    test interfaces

--- interfaces/StudentInterface.ts
+++ interfaces/StudentInterface.ts
@@ -1,10 +1,13 @@
 export default interface StudentInterface {
-    student_id: number;
-    account_id: number;
-    school_id: string;
-    first_name: string;
-    last_name: string;
-    year_level: number;
-    is_enrolled: number;
-    section: number;
+  student_id: number;
+  account_id: number;
+  school_id: string;
+  first_name: string;
+  last_name: string;
+  parent_guardian_id: number;
+  returner_id: number;
+  section_assigned_id: number;
+  year_level: number;
+  is_enrolled: number;
+  section: number;
 }
